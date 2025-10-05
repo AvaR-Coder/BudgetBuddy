e4f8cd8bcd2ca9248c676a8655642c489791c22e
Tannrk <tanmanners@gmail.com>
2025-10-05 19:43:43 -0400

    Update sqliteInsertion.py

--- userInfo/sqliteInsertion.py
+++ userInfo/sqliteInsertion.py
@@ -4,8 +4,8 @@ conn = sqlite3.connect('customers.db')
 cursor = conn.cursor()
 
 cursor.execute(
- "INSERT INTO customers (firstName, lastName, email, password, balance, budget, expences, savings) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
- (firstName, lastName, email, password, balance, budget, expences, savings)
+ "INSERT INTO customers (name, email, password, balance, budget, expences, savings) VALUES (?, ?, ?, ?, ?, ?, ?)",
+ (name, email, password, balance, budget, expences, savings)
 )
 conn.commit()
 conn.close()
