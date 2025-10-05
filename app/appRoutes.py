5969b72933380f19742c62fdbdd3c675991a207a
Tannrk <tanmanners@gmail.com>
2025-10-05 18:00:24 -0400

    Update appRoutes.py

--- app/appRoutes.py
+++ app/appRoutes.py
@@ -9,9 +9,12 @@ def connectDB():
   conn.row_factory = sqlite3.Row
   return conn
 
-@app.route('/')
-def helloTest():
-  return "Hello World"
+@app.route("/", methods=["GET", "POST"])
+def index():
+    name = None
+    if request.method == "POST":
+        name = request.form["username"]
+    return render_template_string(form_html, name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
