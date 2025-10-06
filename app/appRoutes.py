214588c089fc872e3d39aa2d74f5e85b24def871
Tannrk <tanmanners@gmail.com>
2025-10-05 21:10:50 -0400

    Update appRoutes.py

--- app/appRoutes.py
+++ app/appRoutes.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template_string
-from functions import checkPasswordStrength, hashThePassword, connectDB
+from functions import checkPasswordStrength, hashThePassword, connectDB, insertCustomerData
 
 app = Flask(__name__)
 
