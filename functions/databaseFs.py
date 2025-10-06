6b163b0104fcbcf7a59d4861f02f10060d15504a
Tannrk <tanmanners@gmail.com>
2025-10-05 21:09:38 -0400

    Update databaseFs.py

--- functions/databaseFs.py
+++ functions/databaseFs.py
@@ -1,4 +1,22 @@
+import sqlite3
+
 def connectDB():
   conn = sqlite3.connect("customers.db")
   conn.row_factory = sqlite3.Row
   return conn
+
+def insertCustomerData(name, email, password, balance, budget, expences, savings):
+  conn = connectDB()
+  try:
+    cursor = conn.cursor()
+    cursor.execute(
+      "INSERT INTO customers (name, email, password, balance, budget, expences, savings) VALUES (?, ?, ?, ?, ?, ?, ?)",
+      (name, email, password, balance, budget, expences, savings)
+    )
+    conn.commit()
+    return True
+  except sqlite3.Error as e:
+    return False
+  finally:
+    conn.close()
+  
